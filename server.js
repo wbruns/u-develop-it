db41b624a5d58de347c0df58660f110e761d095a
wbruns <wbruns07@gmail.com>
2022-03-08 19:35:36 -0600

    set up connection to database and queried the candidates table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// import express
+// import express and mysql2
 const express = require('express');
+const mysql = require('mysql2');
 
 // set port and app
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        password: '0077Coke',
+        database: 'election'
+    },
+    console.log('Connected to the election databs')
+);
+
+// query for data
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
